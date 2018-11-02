376643bd6bc3beb070efb46a70b907533134ebe7
Paula Muldoon <pmuldoon@umich.edu>
2018-11-02 12:43:44 +0000

    remove unnecessary variable declarations

--- test/prcTest.js
+++ test/prcTest.js
@@ -8,51 +8,41 @@ const getPRCIndexRecursively = require('./params/getPRCIndexRecursively');
 const isUserAdmin = require('./params/isUserAdmin');
 const ErrorMessages = require('./params/errorMessages');
 
+
 describe('get PRC index', () => {
     describe('get PRC index with loop', () => {
         describe('user indices', () => {
             Object.entries(PrcIndexParams.scenarios()).forEach((scenario) => {
-                const userId = scenario[1].userId;
-                const numberPRCsUsed = scenario[1].numberPRCsUsed;
-                const expected = scenario[1].expected;
                 it('should return an admin index', () => {
-                    const actual = Prc.getPRCIndex(userId, numberPRCsUsed);
-                    expect(actual, `number of PRCs used: ${numberPRCsUsed}`)
-                        .to.eq(expected);
+                    const actual = Prc.getPRCIndex(scenario[1].userId, scenario[1].numberPRCsUsed);
+                    expect(actual, `number of PRCs used: ${scenario[1].numberPRCsUsed}`)
+                        .to.eq(scenario[1].expected);
                 });
             });
         });
 
         describe('error handling', () => {
             Object.entries(PrcIndexParams.errorScenarios()).forEach((errorScenario) => {
-                const userId = errorScenario[1].userId;
-                const numberPRCsUsed = errorScenario[1].numberPRCsUsed;
                 expect(() => Prc.getPRCIndex(
-                    userId,
-                    numberPRCsUsed,
+                    errorScenario[1].userId,
+                    errorScenario[1].numberPRCsUsed,
                 )).to.throw(ErrorMessages.errorMessages().noPrcError);
             });
         });
     });
 
     describe('get PRC index recursively', () => {
-        let i;
-        let n;
-        beforeEach(() => {
-            i = getPRCIndexRecursively.sharedData().i;
-            n = getPRCIndexRecursively.sharedData().n;
-        });
-
         describe('user indices', () => {
             Object.entries(PrcIndexParams.scenarios()).forEach((scenario) => {
-                const userId = scenario[1].userId;
-                const numberPRCsUsed = scenario[1].numberPRCsUsed;
-                const expected = scenario[1].expected;
-
                 it('should return a user index', () => {
-                    const actual = Prc.getPRCIndexRecursively(userId, numberPRCsUsed, i, n);
-                    expect(actual, `number of PRCs used: ${numberPRCsUsed}`)
-                        .to.eq(expected);
+                    const actual = Prc.getPRCIndexRecursively(
+                        scenario[1].userId,
+                        scenario[1].numberPRCsUsed,
+                        getPRCIndexRecursively.sharedData().i,
+                        getPRCIndexRecursively.sharedData().n,
+                    );
+                    expect(actual, `number of PRCs used: ${scenario[1].numberPRCsUsed}`)
+                        .to.eq(scenario[1].expected);
                 });
             });
         });
@@ -60,13 +50,11 @@ describe('get PRC index', () => {
         describe('error handling', () => {
             Object.entries(PrcIndexParams.errorScenarios()).forEach((errorScenario) => {
                 it('throws an error if no remaining PRCs', () => {
-                    const userId = errorScenario[1].userId;
-                    const numberPRCsUsed = errorScenario[1].numberPRCsUsed;
                     expect(() => Prc.getPRCIndexRecursively(
-                        userId,
-                        numberPRCsUsed,
-                        i,
-                        n,
+                        errorScenario[1].userId,
+                        errorScenario[1].numberPRCsUsed,
+                        getPRCIndexRecursively.sharedData().i,
+                        getPRCIndexRecursively.sharedData().n,
                     )).to.throw(ErrorMessages.errorMessages().noPrcError);
                 });
             });
@@ -76,8 +64,7 @@ describe('get PRC index', () => {
     describe('is user admin', () => {
         Object.entries(isUserAdmin.scenarios()).forEach((scenario) => {
             it('returns an admin index', () => {
-                const userId = scenario[1].userId;
-                expect(Prc.isUserAdmin(userId)).to.eq(scenario[1].expected);
+                expect(Prc.isUserAdmin(scenario[1].userId)).to.eq(scenario[1].expected);
             });
         });
     });
